276ae2d90e1030c2f34fa46c5513fb5c40f7d2a3
ogulcandulger <ogulcan.dulger@gmail.com>
2022-03-26 11:21:19 +0300

    env

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {process.env.NODE_ENV}
         </a>
       </header>
     </div>
