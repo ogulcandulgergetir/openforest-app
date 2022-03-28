8ec47336186a84594344994c5d45ccfed7b5262b
ogulcandulger <ogulcan.dulger@gmail.com>
2022-03-28 11:59:05 +0300

    env variable changed

--- src/Constants.js
+++ src/Constants.js
@@ -4,8 +4,8 @@ const dev = {
 }
 
 const prod = {
-    MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN,
-    MAPBOX_STYLE_URL: process.env.MAPBOX_STYLE_URL
+    MAPBOX_ACCESS_TOKEN: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
+    MAPBOX_STYLE_URL: process.env.REACT_APP_MAPBOX_STYLE_URL
 } 
 
 export const config = process.env.NODE_ENV === "development" ? dev : prod;
